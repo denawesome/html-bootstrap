c6d66d29bb633faed1dfd87b2651e94a32e3d94a
denawesome <mwtemplates@ukr.net>
2020-07-22 17:10:59 +0300

    fix indent

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,14 @@ var browserSync = require('browser-sync').create();
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function (done) {
- return gulp.src('./sass/**/*.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass().on('error', sass.logError))
-  .pipe(sourcemaps.write('./maps'))
-  .pipe(gulp.dest('./css'))
-  .pipe(browserSync.stream({match: '**/*.css'}));
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream({match: '**/*.css'}));
 
-  done();
+    done();
 });
 
 gulp.task('serve', gulp.series('sass', function(done) {
